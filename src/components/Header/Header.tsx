15eefc806510a793408bcc53cf1bdf4bf4597564
konikasharma99 <konikadutta2991@gmail.com>
2024-10-23 18:23:30 +0530

    Added search functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,10 @@
 import styles from "../Header/Header.module.css";
-import { SEARCH_BAR_LOGO, LOGO_URL } from "../../utils/common";
+import { LOGO_URL } from "../../utils/common";
+
 const Header = () => {
   return (
     <div className={styles.headerMain}>
       <img className={styles.headerLogo} src={LOGO_URL} />
-      <div className={styles.searchBox}>
-        <input className={styles.searchInput} placeholder="Search for food" />
-        <img className={styles.searchLogo} src={SEARCH_BAR_LOGO} />
-      </div>
       <ul className={styles.linksList}>
         <li>Order now</li>
         <li>About us</li>
--- src/components/RestaurantCard/RestaurantCard.tsx
+++ src/components/RestaurantCard/RestaurantCard.tsx
@@ -1,14 +1,24 @@
 import styles from "./RestaurantCard.module.css";
-import { CDN_URL } from "../../utils/common";
+import { CDN_URL, SEARCH_BAR_LOGO } from "../../utils/common";
 import { useState } from "react";
 interface IProps {
   cardProps: any;
 }
 const RestaurantCard = (props: IProps) => {
+  const [searchRestaurant, setSearchRestaurant] = useState("");
   const imgUrl = CDN_URL;
   const [restaurants, setRestaurants] = useState(props.cardProps);
+  const [allRestaurants] = useState(props.cardProps);
   const filterRestsAbove4 = () => {
     setRestaurants(restaurants.filter((e) => e.info.avgRatingString > 4.3));
+  };
+  const searchRestaurantList = () => {
+    console.log("**", searchRestaurant);
+    setRestaurants(
+      allRestaurants.filter((e) =>
+        e.info.name.toLowerCase().includes(searchRestaurant.toLowerCase())
+      )
+    );
   };
   const sortOnRating = () => {
     const sortByStars = [...restaurants];
@@ -31,6 +41,20 @@ const RestaurantCard = (props: IProps) => {
         <button className={styles.sortButton} onClick={() => sortOnRating()}>
           <p className={styles.sortButtonText}>Sort Restaurants on rating</p>
         </button>
+        <div className={styles.searchBox}>
+          <input
+            className={styles.searchInput}
+            placeholder="Search for food"
+            onChange={(e) => {
+              setSearchRestaurant(e.target.value);
+            }}
+          />
+          <img
+            onClick={() => searchRestaurantList()}
+            className={styles.searchLogo}
+            src={SEARCH_BAR_LOGO}
+          />
+        </div>
       </div>
       <div className={styles.allRestaurants}>
         {restaurants.map((cardObj: any) => {
